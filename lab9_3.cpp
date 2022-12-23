489aa92dc6ea3c4179e7984155cfa7d81aba3d82
bbam <nunnapats1822@gmail.com>
2022-12-23 16:14:42 +0700

    bblab9_3completed

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,13 +1,41 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main(){
+    int age,bounty;
+    string cha;
+    cout<<"Enter your age: ";
+    cin>>age;
+    if(age <= 25){
+        double height;
+        cout<<"Enter your height: ";
+        cin>>height;
+            if(height < 100){
+                cha = "Chopper";    
+            }else if(height < 180){
+                cha = "Usopp";}      
+            else{
+                cout<<"Enter your bounty: ";
+                cin>>bounty;
+                    if(bounty <= 1.1*pow(10,9)){
+                        cha = "Sanji"; 
+                    }else{
+                        cha = "Zoro";     
+                    }
+            }
+    }else{
+        if(age <= 60){
+            cout<<"Enter your bounty: ";
+            cin>>bounty;
+                if(bounty <= 5*pow(10,8)){
+                    cha = "Franky"; 
+                }else{
+                    cha = "Jinbe";     
+                }
+        }else{
+            cha = "Brook";      
+        }    
+    }
+cout<<"Your character = "<<cha;
+}
